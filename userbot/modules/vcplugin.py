249dfe9a5eb22e239eebcc230088f9f2ea5a308c
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-30 05:48:36 +0700

    Update vcplugin.py

--- userbot/modules/vcplugin.py
+++ userbot/modules/vcplugin.py
@@ -25,7 +25,7 @@ from userbot import CMD_HELP
 from userbot import PLAY_PIC as fotoplay
 from userbot import QUEUE_PIC as ngantri
 from userbot import call_py, owner
-from userbot.utils import bash, edit_delete, edit_or_reply, kyy_cmd
+from userbot.utils import bash, edit_delete, edit_or_reply, hiro_cmd
 from userbot.utils.chattitle import CHAT_TITLE
 from userbot.utils.queues.queues import (
     QUEUE,
@@ -112,7 +112,7 @@ async def skip_current_song(chat_id: int):
     return [songname, link, type]
 
 
-@kyy_cmd(pattern="play(?:\\s|$)([\\s\\S]*)")
+@hiro_cmd(pattern="play(?:\\s|$)([\\s\\S]*)")
 async def vc_play(event):
     title = event.pattern_match.group(1)
     replied = await event.get_reply_message()
@@ -204,7 +204,7 @@ async def vc_play(event):
                 await botman.edit(f"`{ep}`")
 
 
-@kyy_cmd(pattern="vplay(?:\\s|$)([\\s\\S]*)")
+@hiro_cmd(pattern="vplay(?:\\s|$)([\\s\\S]*)")
 async def vc_vplay(event):
     title = event.pattern_match.group(1)
     replied = await event.get_reply_message()
@@ -357,7 +357,7 @@ async def vc_vplay(event):
                     await xnxx.edit(f"`{ep}`")
 
 
-@kyy_cmd(pattern="end$")
+@hiro_cmd(pattern="end$")
 async def vc_end(event):
     chat_id = event.chat_id
     if chat_id in QUEUE:
@@ -371,7 +371,7 @@ async def vc_end(event):
         await edit_delete(event, "**Tidak Sedang Memutar Streaming**")
 
 
-@kyy_cmd(pattern="skip(?:\\s|$)([\\s\\S]*)")
+@hiro_cmd(pattern="skip(?:\\s|$)([\\s\\S]*)")
 async def vc_skip(event):
     chat_id = event.chat_id
     if len(event.text.split()) < 2:
@@ -400,7 +400,7 @@ async def vc_skip(event):
             await event.edit(DELQUE)
 
 
-@kyy_cmd(pattern="pause$")
+@hiro_cmd(pattern="pause$")
 async def vc_pause(event):
     chat_id = event.chat_id
     if chat_id in QUEUE:
@@ -413,7 +413,7 @@ async def vc_pause(event):
         await edit_delete(event, "**Tidak Sedang Memutar Streaming**")
 
 
-@kyy_cmd(pattern="resume$")
+@hiro_cmd(pattern="resume$")
 async def vc_resume(event):
     chat_id = event.chat_id
     if chat_id in QUEUE:
@@ -426,7 +426,7 @@ async def vc_resume(event):
         await edit_delete(event, "**Tidak Sedang Memutar Streaming**")
 
 
-@kyy_cmd(pattern=r"volume(?: |$)(.*)")
+@hiro_cmd(pattern=r"volume(?: |$)(.*)")
 async def vc_volume(event):
     query = event.pattern_match.group(1)
     chat = await event.get_chat()
@@ -449,7 +449,7 @@ async def vc_volume(event):
         await edit_delete(event, "**Tidak Sedang Memutar Streaming**")
 
 
-@kyy_cmd(pattern="playlist$")
+@hiro_cmd(pattern="playlist$")
 async def vc_playlist(event):
     chat_id = event.chat_id
     if chat_id in QUEUE:
@@ -478,7 +478,7 @@ async def vc_playlist(event):
 # FROM GeezProjects < https://github.com/vckyou/GeezProjects \>
 # ambil boleh apus credits jangan ya ka:)
 
-@kyy_cmd(pattern="joinvc(?: |$)(.*)")
+@hiro_cmd(pattern="joinvc(?: |$)(.*)")
 async def join_(event):
     xnxx = await edit_or_reply(event, f"**Processing**")
     if len(event.text.split()) > 1:
@@ -497,15 +497,15 @@ async def join_(event):
         AudioPiped(
             'http://duramecho.com/Misc/SilentCd/Silence01s.mp3'
         ),
-        stream_type=StreamType().pulse_stream,
+        stream_type=StreamType().pulse_stream, 
     )
     try:
-        await xnxx.edit("**{} Berhasil Naik Ke VCG** `{}`".format(owner, str(event.chat_id)))
+        await xnxx.edit("**{} Fake VCG Telah Berhasil Naik** `{}`".format(owner, str(event.chat_id)))
     except Exception as ex:
         await edit_delete(event, f"**ERROR:** `{ex}`")
 
 
-@kyy_cmd(pattern="leavevc(?: |$)(.*)")
+@hiro_cmd(pattern="leavevc(?: |$)(.*)")
 async def leavevc(event):
     """ leave video chat """
     xnxx = await edit_or_reply(event, "Processing")
@@ -514,11 +514,11 @@ async def leavevc(event):
     if from_user:
         try:
             await call_py.leave_group_call(chat_id)
-        except (NotInGroupCallError, NoActiveGroupCall):
+        except (NotInGroupCallError, NoActiveGroupCall): 
             pass
-        await xnxx.edit("**{} Berhasil Turun Dari VCG** `{}`".format(owner, str(event.chat_id)))
+        await xnxx.edit("**{} Fake VCG Berhasil Turun** `{}`".format(owner, str(event.chat_id)))
     else:
-        await edit_delete(event, f"**Maaf {owner} Tidak Berada Di VCG**")
+        await edit_delete(event, f"**Sorry {owner} Tidak Berada Di VCG**")
 
 
 @call_py.on_stream_end()
