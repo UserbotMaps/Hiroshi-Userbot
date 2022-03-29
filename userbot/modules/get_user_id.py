fc6770232a0289ca15b98710abcb5b97f923072e
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-29 12:13:39 +0700

    Update get_user_id.py

--- userbot/modules/get_user_id.py
+++ userbot/modules/get_user_id.py
@@ -1,39 +1,39 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot.utils import edit_or_reply, edit_delete, kyy_cmd
+from userbot.utils import edit_or_reply, edit_delete, hiro_cmd
 from userbot import bot, CMD_HELP, CMD_HANDLER as cmd
 
 
-@kyy_cmd(pattern="getid(?: |$)(.*)")
+@hiro_cmd(pattern="getid(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await edit_delete(event, "`Mohon Reply Ke Pesan`")
+        await edit_delete(event, "`Mohon Reply Dulu Pesannya Kontol`")
         return
     reply_message = await event.get_reply_message()
     if not reply_message.text:
-        await edit_delete(event, "```Mohon Balas Ke Reply```")
+        await edit_delete(event, "```Mohon Di Balas Ke Replynya```")
         return
     chat = "@getidsbot"
     reply_message.sender
     if reply_message.sender.bot:
-        await edit_delete(event, "`Mohon Reply Ke Pesan`")
+        await edit_delete(event, "`Mohon Reply Dulu Pesannya Kontol`")
         return
-    xx = await edit_or_reply(event, "`Mencari ID.......`")
+    xx = await edit_or_reply(event, "` Sabar Sedang Mencari ID.......`")
     async with bot.conversation(chat) as conv:
         try:
             response = conv.wait_event(
                 events.NewMessage(
                     incoming=True,
-                    from_users=1663258664))
+                    from_users=1821140802))
             await bot.forward_messages(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await event.reply("`Bot Sedang Error`")
+            await event.reply("`Bot Lu Sedang Error Mampus`")
             return
         if response.text.startswith("Forward"):
-            await xx.edit("`Mohon Maaf, Orang Ini Tidak Mempunyai ID`")
+            await xx.edit("`Mohon Maaf, Orang Kontol Ini Tidak Mempunyai ID Karena Dia Yatim`")
         else:
             await xx.edit(f"{response.message.message}")
 
