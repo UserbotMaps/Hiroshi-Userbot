7de05556bd8833c4775e22cae16201cbc0996c2f
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-04-01 02:00:20 +0700

    Update asupan.py

--- userbot/modules/asupan.py
+++ userbot/modules/asupan.py
@@ -1,13 +1,19 @@
-# 🍀 © @tofik_dn
+# © @tofik_dn
 # ⚠️ Do not remove credits
+# recode by @greyyvbss
+# video lucu & sad by @JustRex
+# video bkp @Bisubiarenak
 
+import random
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot.utils import hiro_cmd
-import random
+
 from userbot import owner
 from telethon.tl.types import InputMessagesFilterVideo
+from telethon.tl.types import InputMessagesFilterVoice
+from telethon.tl.types import InputMessagesFilterPhotos
 
 
 @hiro_cmd(pattern="asupan$")
@@ -19,35 +25,75 @@ async def _(event):
                 "@testimonihiroshi", filter=InputMessagesFilterVideo
             )
         ]
-        aku = await event.client.get_me()
+        aing = await event.client.get_me()
         await event.client.send_file(
             event.chat_id,
             file=random.choice(asupannya),
-            caption=f"**Asupan by** [{aku.first_name}](tg://user?id={aku.id})")
-
+            caption=f"**Nih Sayang asupannya** [{owner}](tg://user?id={aing.id})",
+        )
         await event.delete()
     except Exception:
-        await event.edit("**Tidak dapat menemukan video asupan.**")
+        await event.edit("**Tidak bisa menemukan video asupan.**")
 
 
 @hiro_cmd(pattern="desah$")
 async def _(event):
     try:
-        desahannya = [
+        desahnya = [
             desah
             async for desah in event.client.iter_messages(
                 "@deshanhiroshi", filter=InputMessagesFilterVoice
             )
         ]
-        aku = await event.client.get_me()
+        aing = await event.client.get_me()
         await event.client.send_file(
             event.chat_id,
-            file=random.choice(desahannya),
-            caption=f"**Desahan by** [{aku.first_name}](tg://user?id={aku.id})")
-
+            file=random.choice(desahnya),
+            caption=f"**Jangan Sange ya kak, nih desah buat** [{owner}](tg://user?id={aing.id})",
+        )
         await event.delete()
     except Exception:
-        await event.edit("**Tidak dapat menemukan vn desah.**")
+        await event.edit("**sange nya ditahan dulu ya kak.**")
+
+
+@hiro_cmd(pattern="bokep$")
+async def _(event):
+    try:
+        desahnya = [
+            bokep
+            async for bokep in event.client.iter_messages(
+                "@asupanskyzu", filter=InputMessagesFilterVoice
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(bokepnya),
+            caption=f"**Jangan coli terus kak, nih bokep buat** [{owner}](tg://user?id={aing.id})",
+        )
+        await event.delete()
+    except Exception:
+        await event.edit("**bokep nya ditahan dulu ya kak.**")
+
+
+@hiro_cmd(pattern="vidlucu$")
+async def _(event):
+    try:
+        vidlucunya = [
+            vidlucu
+            async for vidlucu in event.client.iter_messages(
+                "@videolucuxauserbot", filter=InputMessagesFilterVideo
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(vidlucunya),
+            caption=f"**Nih kak video lucunya** [{owner}](tg://user?id={aing.id})",
+        )
+        await event.delete()
+    except Exception:
+        await event.edit("**Tidak bisa menemukan video lucu.**")
 
 
 @hiro_cmd(pattern="ayang$")
@@ -56,29 +102,54 @@ async def _(event):
         ayangnya = [
             ayang
             async for ayang in event.client.iter_messages(
-                "@IndomieGantengV2", filter=InputMessagesFilterPhotos
+                "@CeweLogoPack", filter=InputMessagesFilterPhotos
             )
         ]
-        aku = await event.client.get_me()
+        aing = await event.client.get_me()
         await event.client.send_file(
             event.chat_id,
             file=random.choice(ayangnya),
-            caption=f"**Ayang by** [{aku.first_name}](tg://user?id={aku.id})")
-
+            caption=f"**Ayang nya** [{owner}](tg://user?id={aing.id})",
+        )
         await event.delete()
     except Exception:
-        await event.edit("**GA ADA YANG MAU SAMA LO, MAKANYA GANTENK.**")
+        await event.edit("**GA ADA YANG MAU SAMA LO, MAKANYA CAKEP!.**")
 
+
+@hiro_cmd(pattern="sadvid$")
+async def _(event):
+    try:
+        sadvidnya = [
+            sadvid
+            async for sadvid in event.client.iter_messages(
+                "@sadvideorexa", filter=InputMessagesFilterVideo
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(sadvidnya),
+            caption=f"**Jangan Terlalu Sedih ya kak** [{owner}](tg://user?id={aing.id})",
+        )
+        await event.delete()
+    except Exception:
+        await event.edit("Maaf, kayaknya kamu ga pantes untuk sedih :) .")
 
 CMD_HELP.update(
     {
-        "asupan": f"**Plugin : **`asupan`\
-        \n\n  •  **Syntax :** `{cmd}asupan`\
-        \n  •  **Function : **Untuk Mengirim video asupan secara random.\
-        \n\n  •  **Syntax :** `{cmd}desah`\
-        \n  •  **Function : **Untuk Mengirim voice desah secara random.\
-        \n\n  •  **Syntax :** `{cmd}ayang`\
-        \n  •  **Function : **Untuk Mencari ayang buat cowok yang jomblo.\
-    "
+        "asupan": f"**Plugin : **asupan\
+        \n\n  •  **𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝 :** {cmd}asupan\
+        \n  ☆  **𝙁𝙪𝙣𝙜𝙨𝙞 : **Untuk Mengirim video asupan secara random.\
+        \n\n  •  **𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝 :** {cmd}desah\
+        \n  ☆  **𝙁𝙪𝙣𝙜𝙨𝙞 : **Untuk Mengirim voice desah secara random.\
+        \n\n  •  **𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝 :** {cmd}bokep\
+        \n  ☆  **𝙁𝙪𝙣𝙜𝙨𝙞 : **Untuk Mengirim video bokep secara random.\
+        \n\n  •  **𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝 :** {cmd}vidlucu\
+        \n  ☆  **𝙁𝙪𝙣𝙜𝙨𝙞 : **Untuk Mengirim video lucu secara random.\
+        \n\n  •  **𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝 :** {cmd}ayang\
+        \n  ☆  **𝙁𝙪𝙣𝙜𝙨𝙞 : **Untuk Mendapatkan Ayang mu, hehe.\
+        \n\n  •  **𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝 :** {cmd}sadvid\
+        \n  ☆  **𝙁𝙪𝙣𝙜𝙨𝙞 : **Untuk Melihat video sad random.\
+"
     }
 )
