f18fd7dead35ca1123e2a57e7d7eb8d476e2cfd9
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-04-24 02:40:09 +0700

    Update asupan.py

--- userbot/modules/asupan.py
+++ userbot/modules/asupan.py
@@ -62,7 +62,7 @@ async def _(event):
         bokepnya = [
             bokep
             async for bokep in event.client.iter_messages(
-                "@bokephiroshi", filter=InputMessagesFilterVideo
+                "@fakyudurov", filter=InputMessagesFilterVideo
             )
         ]
         aing = await event.client.get_me()
